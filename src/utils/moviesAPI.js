2e35a6f58578516dc9380b28c640f1ae1fb3174c
abhi <abhi.badam186@gmail.com>
2024-06-14 20:52:03 +0530

    getting data of movies using tmdb api

---
+++ src/utils/moviesAPI.js
@@ -0,0 +1,8 @@
+
+export const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZDc4YTMwMGI1Nzg4YjQ1MjdjZDJkZWI5YzhjMTZkNSIsInN1YiI6IjY2NmI0ZjQxYmM2OTc3NGVkMGE3YTA3OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OxvL92s-f7IVyJS8cD4KE6jCazfNNqihHYDpWDpjWYw'
+  }
+};
